8cec850ebb6c1837404fd9a5cf3f71b539b4d464
Fatz-Dev <fatazikrillah007@gmail.com>
2026-04-16 12:23:40 +0700

    materi tugas dirumah

---
+++ meteri12/android/app/src/main/kotlin/com/example/meteri12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.meteri12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ meterihttpcomplete/android/app/src/main/kotlin/com/example/meterihttpcomplete/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.meterihttpcomplete
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
