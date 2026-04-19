1c75a753fd1b0c56194fbfd8161305e642675588
Fatz-Dev <fatazikrillah007@gmail.com>
2026-04-19 17:54:56 +0700

    Materi 11, 12, Api Al-quran

---
+++ meterihttpcomplete/lib/detil.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'surat.dart';
+
+class DetailPage extends StatelessWidget {
+  final Surat surat;
+
+  DetailPage({required this.surat});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(surat.nama),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nomor Surat: ${surat.nomor}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Nama Surat: ${surat.nama}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Nama Latin: ${surat.latin}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Jumlah Ayat: ${surat.jumlahAyat}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Penjelasan:',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              surat.tafsirSurat,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
